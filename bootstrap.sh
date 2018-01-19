57d189ba7fc7a34197c73a7dd40eff7fb0562074
SeanDMR <sean@flyinggrizzly.io>
2018-01-19 22:53:23 +0000

    Add zsh theme file, setup script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln -s zshrc ~/.zshrc
+ln -s powerlevel9krc ~/.powerleve
+ln -s vimrc ~/.vimrc
+ln -s irbrc ~/.irbrc
+
+ln -s gitconfig ~/.gitconfig
+
+ln -s default-gems ~/.rbenv/default-gems
+ln -s rubocop.yml ~/.rubocop.yml
+
